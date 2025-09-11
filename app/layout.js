04b50825ce5483e2417c5f19748b5cc261f1d6c3
RukshanNuwan <shanwijendra.dev@gmail.com>
2025-09-11 22:30:11 +0530

    fix: remove Poppins font import and usage from layout component

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "./providers";
-
-const poppins = Poppins({
-  variable: "--font-poppins",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata = {
   title: "Inventory Management System",
@@ -18,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${poppins} antialiased`}>
+        <body className={`antialiased`}>
           <ReduxProvider>
             {children}
             <Toaster />
